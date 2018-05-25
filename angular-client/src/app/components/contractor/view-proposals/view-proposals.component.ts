68de9f94df201d3d9572c4d41e393c22f65409fd
leonardoespinosa <leonardoespinosa@realbind.com>
2018-05-25 10:14:48 -0500

    Modify view proposals page

--- angular-client/src/app/components/contractor/view-proposals/view-proposals.component.ts
+++ angular-client/src/app/components/contractor/view-proposals/view-proposals.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CurrentUser } from '../../../models/access/access.model';
+import { AccessService } from '../../../services/access/access.service';
 
 @Component({
     selector: 'view-proposals',
@@ -6,13 +8,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
     styleUrls: ['./view-proposals.component.scss']
 })
 export class ViewProposalsComponent implements OnInit, OnDestroy {
-    constructor(){}
 
-    ngOnInit(){
+    private _currentUser: CurrentUser;
+    private _inProcess: boolean = false;
+    private _solicitudSelect: Object = { nameRequest: "No hay solicitudes creadas" }
+
+    /**
+     * ViewProposalsComponent Constructor
+     * @param {AccessService} _accessService 
+     */
+    constructor(private _accessService: AccessService) {
 
     }
 
-    ngOnDestroy(){
-        
+    ngOnInit() {
+        this._currentUser = this._accessService.getCurrentUser();
+    }
+
+    ngOnDestroy() {
+
     }
 }
